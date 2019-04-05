b35502792388384a2fed447fecdeddadea61c78c
Monty <tuckerjsarah@gmail.com>
2019-04-04 21:30:15 -0400

    added stuff

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -129,6 +129,7 @@ class Navbar extends Component {
       <input onChange= {this.searchOnChange} className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
       <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
+    <img className= 'myImg'></img>
   </div>
 </nav>
         )
