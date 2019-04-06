8717ef6134a07ed853bbf5a8753ddde170a9839f
Jacob Sipple <sipple3@gmail.com>
2019-04-05 20:24:04 -0400

    movied static.json

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Authenticator from './Authenticator'
 import * as serviceWorker from './serviceWorker';
-import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Authenticator />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
