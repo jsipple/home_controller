f8ab91469e2e745b4b522bc6b5ef006727f76ee0
Jacob Sipple <sipple3@gmail.com>
2019-05-26 09:58:15 -0400

    removed manage your devices

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -135,9 +135,6 @@ class Navbar extends Component {
 
       </li>
       <li className="nav-item active">
-        <a className="nav-link" href="https://home-controller12345.herokuapp.com">Manage your devices<span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item active">
         <Link className="nav-link" to="/tutorial">Getting started<span className="sr-only">(current)</span></Link>
       </li>
       <li className="nav-item active">
